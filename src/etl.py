fea46c593c299cc4bd829161634fc8c18b12929b
Peter Wainaina <petwah17@gmail.com>
2023-05-01 11:22:31 +0300

    refactor code for better flow

--- src/etl.py
+++ src/etl.py
@@ -7,13 +7,13 @@ from pyspark.sql.functions import lit
 from dotenv  import load_dotenv
 import logging
 
-## Initiate dotenv
+## Initiate dotenvcd
 load_dotenv()
 
 
 
 
-def load_contract_offers(source_directory: str, destination_directory: str) -> None:
+def load_contract_offer(server_name: str, database_name: str, username: str, password: str) -> None:
     """
     Ingests CSV files from a source directory into a SQL Server database, and moves the ingested files to a
     destination directory. The function records the ingested file names in a record file, and updates an audit table
@@ -28,19 +28,19 @@ def load_contract_offers(source_directory: str, destination_directory: str) -> N
     :param password: The password to use to connect to the SQL Server database.
     :return: None
     """
+    print("i am here")
     try:
+        print("i am here")
         # create a Spark session
         spark = SparkSession.builder.appName("CSV Ingestion").getOrCreate()
-        # create the audit table if it doesn't exist
-        # spark.sql(f"CREATE TABLE IF NOT EXISTS {database_name}.audit (file_path STRING, row_count LONG, status STRING, ingestion_time TIMESTAMP)")
         # directory containing CSV files
-        source_directory = r"C:\Users\Administrator\Documents\Oneacre\Interview\datasource"
+        source_directory = os.getenv("source_directory")
 
         # directory to move CSV files to
-        destination_directory = r"C:\Users\Administrator\Documents\Oneacre\Interview\destination"
+        destination_directory = os.getenv("destination_directory")
 
         # file to record ingested files
-        record_file = r"C:\Users\Administrator\Documents\Oneacre\Interview\destination\file.txt"
+        record_file = os.getenv("record_file")
         # list of CSV file names in the source directory
         csv_files = [f for f in os.listdir(source_directory) if f.endswith("offers.csv")]
         # read in the record file to a set
@@ -57,6 +57,18 @@ def load_contract_offers(source_directory: str, destination_directory: str) -> N
                 df = spark.read.csv(file_path, header=True, inferSchema=True)
                 df.show()
 
+                
+                # write the DataFrame to SQL Server
+                df_with_timestamp = df.withColumn("ingestion_time", lit(ingestion_time))
+                df_with_timestamp.show()
+                df_with_timestamp.write.format("jdbc") \
+                    .option("url", f"jdbc:sqlserver://{server_name};database={database_name}") \
+                    .option("dbtable", "stg.contract_offer")\
+                    .option("user", username) \
+                    .option("password", password) \
+                    .mode("append") \
+                    .save()
+                
                 # move the CSV file to the destination directory
                 destination_path = os.path.join(destination_directory, file)
                 shutil.move(file_path, destination_path)
@@ -64,45 +76,38 @@ def load_contract_offers(source_directory: str, destination_directory: str) -> N
                 # add the ingested file to the record file
                 with open(record_file, "a") as f:
                     f.write(file + "\n")
+        # create the audit table if it doesn't exist
+        spark.sql(f"CREATE TABLE IF NOT EXISTS {database_name}.audit (file_path STRING, row_count LONG, status STRING, ingestion_time TIMESTAMP)")
+        # insert a new row into the audit table with success status and timestamp
+        row_count = df.count()
+        ingestion_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        audit_df = spark.createDataFrame([(file_path, row_count, "success", ingestion_time)],
+                                        ["file_path", "row_count", "status", "ingestion_time"])
+        audit_df.write.format("jdbc") \
+            .option("url", f"jdbc:sqlserver://{server_name};database={database_name}") \
+            .option("dbtable", "stg.audit") \
+            .option("user", username) \
+            .option("password", password) \
+            .mode("append") \
+            .save()
 
-                # insert a new row into the audit table with success status and timestamp
-                row_count = df.count()
-                ingestion_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                audit_df = spark.createDataFrame([(file_path, row_count, "success", ingestion_time)],
-                                                ["file_path", "row_count", "status", "ingestion_time"])
-                audit_df.write.format("jdbc") \
-                    .option("url", f"jdbc:sqlserver://{server_name};database={database_name}") \
-                    .option("dbtable", "audit") \
-                    .option("user", username) \
-                    .option("password", password) \
-                    .mode("append") \
-                    .save()
-
-                # write the DataFrame to SQL Server
-                df_with_timestamp = df.withColumn("ingestion_time", lit(ingestion_time))
-                df_with_timestamp.show()
-                df_with_timestamp.write.format("jdbc") \
-                    .option("url", f"jdbc:sqlserver://{server_name};database={database_name}") \
-                    .option("dbtable", "your_table")\
-                    .option("user", username) \
-                    .option("password", password) \
-                    .mode("append") \
-                    .save()
     except Exception as e:
         # log the error
         logging.error(f"An error occurred during ingestion: {str(e)}")
 
     finally:
         spark.stop()
+def tst():
+    print("yes")
 
-    if __name__ == "__main__":
-        
-        # SQL Server database credentials
-        server_name = "your_server_name"
-        database_name = "your_database_name"
-        jdbc_url = f"jdbc:sqlserver://{server_name};database={database_name}"
-        username = "your_username"
-        password = "your_password"
-        load_contract_offers(source_directory, destination_directory, record_file,
-                                server_name, database_name, username, password) 
+if __name__ == "__main__":
+
+    # SQL Server database credentials
+    server_name = os.getenv("server_name")
+    database_name = os.getenv("database_name")
+    username = os.getenv("username")
+    password = os.getenv("password")
+
+load_contract_offer(server_name, database_name, username, password) 
+tst()
     
